786b3cb1c81b29c00fb796eda80699b765e5eb62
Dylan <dylan.castor06@gmail.com>
2022-07-02 14:34:01 +0200

    Fix audit security and convert number to words

--- src/client/public/views/projects.view.js
+++ src/client/public/views/projects.view.js
@@ -5,6 +5,7 @@ import HTMLElement from "../../global/classes/HTMLElement.js";
 import projectAnims from "../animations/projects.animations.js";
 import Request from "../api/Request.js";
 import projectsContainer from "../containers/projects.container.js";
+import toWords from "../../global/utils/toWords.js"
 
 export async function projectsView() {
   let { result } = await new Request("GET", "/projects").play
@@ -12,8 +13,19 @@ export async function projectsView() {
   let container = projectsContainer()
   main.appendChild(container)
 
+  headerProject(result)
   displayProject(result[0])
   switchProject(result)
+}
+
+function headerProject(project) {
+  let container = document.getElementById('container')
+  let div = new HTMLElement('div', 'headerProject').element
+  let h2 = new HTMLElement('h2').inner(`Project <span id='counter'>${toWords(1)}</span>/<span>${toWords(project.length)}</span>`)
+  let h1 = new HTMLElement('h1', 'title').text(project[0].name)
+  div.append(h2, h1)
+
+  container.appendChild(div)
 }
 
 function displayProject(project) {
@@ -26,12 +38,12 @@ function displayProject(project) {
   subContainer.setAttribute("data-project", project.id)
   subContainer.style.background = `center/cover no-repeat url(./private/static/${project.image})`
 
-  let title = new HTMLElement('h1').text(project.name)
+  title.textContent = project.name
   let desc = new HTMLElement('p').text(project.text)
   let url = new HTMLElement('a').attributes([["href", project.url], ["target", "_blank"]])
   url.textContent = project.url
   let date = new HTMLElement('h3').text(project.date)
-  subContainer.append(title, desc, url, date)
+  subContainer.append(desc, url, date)
 
   projectAnims().play()
 }
@@ -57,13 +69,16 @@ function previousArrow(project) {
 function arrowsControllers(result) {
   let previous = document.getElementById("previous")
   let next = document.getElementById("next")
+  let counter = document.getElementById('counter')
 
   if (next) next.addEventListener("click", () => {
+    counter.textContent = toWords(parseInt(next.dataset.project) + 1)
     displayProject(result[next.dataset.project])
     switchProject(result)
   })
 
   if (previous) previous.addEventListener("click", () => {
+    counter.textContent = toWords(parseInt(previous.dataset.project) + 1)
     displayProject(result[previous.dataset.project])
     switchProject(result)
   })
